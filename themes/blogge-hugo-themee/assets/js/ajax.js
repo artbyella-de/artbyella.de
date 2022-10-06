1154382fee77c61d94c95f736286df26e1b8713a
Lya Schumacher <lya.smr@icloud.com>
2022-10-06 09:48:45 +0200

    fix: doubled id

--- themes/blogge-hugo-themee/assets/js/ajax.js
+++ themes/blogge-hugo-themee/assets/js/ajax.js
@@ -1,4 +1,4 @@
-$(document).on('click','body *', function(){
+$(function(){
     if ($(window).width() < 767) {
         $("li.nav-item").click(function(e){
             e.preventDefault();
@@ -7,17 +7,26 @@ $(document).on('click','body *', function(){
     }
 });
 
-$(document).on('click','body *', function(){
+$(function(){
     if ($(window).width() < 767) {
         $(".clickable").click(function(e){
-            e.preventDefault();
-            loadPage($(this).attr('href'))
-        });
+                e.preventDefault();
+                loadPage($(this).attr('href'))
+            });
+    }
+});
+
+$( window ).on( "load",function(){
+    if ($(window).width() < 767) {
+        $(".clickable").click(function(e){
+                e.preventDefault();
+                loadPage($(this).attr('href'))
+            });
     }
 });
 
 function loadPage(url){
-    $("#content").load(url + " #content");
+    $("#content").load(url + " #content section");
     $('html, body').animate({ scrollTop: 0 }, 0);
     $('button.navbar-toggler').addClass("collapsed");
     $('div.navbar-collapse').removeClass("show");
