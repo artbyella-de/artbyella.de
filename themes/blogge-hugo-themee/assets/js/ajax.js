da0b1282a34afdb772c8a6247bc99c60e3fa58c3
Lya Schumacher <lya.smr@icloud.com>
2022-10-06 09:39:49 +0200

    fix: on click, body *

--- themes/blogge-hugo-themee/assets/js/ajax.js
+++ themes/blogge-hugo-themee/assets/js/ajax.js
@@ -1,21 +1,21 @@
-$( document ).ready(function(){
+$(document).on('click','body *', function(){
     if ($(window).width() < 767) {
         $("li.nav-item").click(function(e){
             e.preventDefault();
             loadPage($(this).attr('href'))
         });
     }
-});
-$( document ).ready(function(){
+
     if ($(window).width() < 767) {
         $(".clickable").click(function(e){
-                e.preventDefault();
-                loadPage($(this).attr('href'))
-            });
+            e.preventDefault();
+            loadPage($(this).attr('href'))
+        });
     }
 });
+
 function loadPage(url){
-    $("#content").replacewith(url + " #content");
+    $("#content").load(url + " #content");
     $('html, body').animate({ scrollTop: 0 }, 0);
     $('button.navbar-toggler').addClass("collapsed");
     $('div.navbar-collapse').removeClass("show");
